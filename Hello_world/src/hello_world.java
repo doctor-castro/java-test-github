f03593e239aa978fd53b6848a51fd021736079d5
doctor-castro <sooraj@umail.ucsb.edu>
2017-02-21 06:06:04 -0800

    Add files via upload

---
+++ Hello_world/src/hello_world.java
@@ -0,0 +1,13 @@
+
+public class hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Hello World \n 3210 \n");
+		System.out.println("---\n###");
+		System.out.print("1. 2. 3. 4. 5.");
+
+	}
+
+}
